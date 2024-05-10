ace274d28ba2ea389ebbdc0a834cc1a2418b4fae
ImanYeganeh <imanyegane30@gmail.com>
2024-05-10 19:33:54 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.scss"
+import Test from "./Test";
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return <div>
+    {/* <section>
+      <Navbar/>    
+    </section>
+    <section>Parallex</section>
+    <section>Services</section>
+    <section>Parallex</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section> */}
+    <Test />
+    <Test/>
+    
+    </div>;
+};
+
+export default App;
